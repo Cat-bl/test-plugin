411f226b03e4b9dae37b3f46d8dde919c1a3fc53
Cat-bl <32174@qq.com>
2025-12-18 16:40:34 +0800

    清理文件

--- constants/core.js
+++
@@ -1,9 +0,0 @@
-import path from "path";
-
-const packageJsonPath = path.resolve(path.join('./plugins', 'test-plugin'));
-
-// 本地资源
-export const CHECK_IMG = `${packageJsonPath}/assets/check@96x100.png`;
-export const DEERPIPE_IMG = `${packageJsonPath}/assets/deerpipe@100x82.png`;
-export const MISANS_FONT = `${packageJsonPath}/assets/MiSans-Regular.ttf`;
-export const PLUGIN_PATH = `${packageJsonPath}`;
